c3be6cd4577785f72be38ff709d9c368d4f0f70b
Mick Potzkai <potzkai@sipgate.de>
2022-10-13 11:58:40 +0200

    refactor: Apply intelijs suggested code changes

--- Code.js
+++ Code.js
@@ -1,11 +1,11 @@
-// Next up: Refactor; Write choosen calender, startdate and enddate on every card; detect open fridays (and retros?)
+// Next up: Refactor; Write chosen calender, startDate and endDate on every card; detect open fridays (and retros?)
 
 function main() {
   const userPropertyStore = PropertiesService.getUserProperties()
   console.log(`CalendarId: ${userPropertyStore.getProperty('calendarId')}`)
   const lastDeveloperSwitchesUpdate = Number(userPropertyStore.getProperty('lastDeveloperSwitchesUpdate'))
-  // TODO: validate that enddate is in the future
-  if (userPropertyStore.getProperty('calendarId') && userPropertyStore.getProperty('startdate') && userPropertyStore.getProperty('enddate')) {
+  // TODO: validate that endDate is in the future
+  if (userPropertyStore.getProperty('calendarId') && userPropertyStore.getProperty('startDate') && userPropertyStore.getProperty('endDate')) {
     if (lastDeveloperSwitchesUpdate < (+new Date()) - (24 * 60 * 60 * 1000)) {
       return buildCard('Set Developers', setDevelopersSection())
     } else {
@@ -24,19 +24,19 @@ function buildCard(cardName, section) {
 }
 
 function chooseDefaultsSection() {
-  let startDate = Number(PropertiesService.getUserProperties().getProperty('startdate'))
-  let endDate = Number(PropertiesService.getUserProperties().getProperty('enddate'))
+  let startDate = Number(PropertiesService.getUserProperties().getProperty('startDate'))
+  let endDate = Number(PropertiesService.getUserProperties().getProperty('endDate'))
   let calendarId = PropertiesService.getUserProperties().getProperty('calendarId')
   const currentDate = +new Date()
   if (!startDate || startDate < currentDate) {
     startDate = currentDate
   }
   if (!endDate || endDate < startDate) {
-    console.log(`startdate: ${startDate}`)
+    console.log(`startDate: ${startDate}`)
     endDate = Number(startDate) + 3 * 24 * 60 * 60 * 1000 // 3 Tage Zeitspanne
-    console.log(`enddate: ${endDate}`)
+    console.log(`endDate: ${endDate}`)
   } else {
-    console.log(`enddate (${new Date(endDate)}) was larger than startdate (${new Date(startDate)})`)
+    console.log(`endDate (${new Date(endDate)}) was larger than startDate (${new Date(startDate)})`)
   }
 
   const startDatePicker = CardService.newDatePicker()
@@ -90,26 +90,26 @@ function setDevelopersSection() {
           .setFunctionName("handleChangeDefaultsButton"));
 
 
-  let startDate = Number(PropertiesService.getUserProperties().getProperty('startdate'))
-  let endDate = Number(PropertiesService.getUserProperties().getProperty('enddate'))
+  let startDate = Number(PropertiesService.getUserProperties().getProperty('startDate'))
+  let endDate = Number(PropertiesService.getUserProperties().getProperty('endDate'))
   let calendarId = PropertiesService.getUserProperties().getProperty('calendarId')
   let developerSwitchChoicesString = PropertiesService.getUserProperties().getProperty('developerSwitchChoices')
   let developerSwitchChoices = {}
 
   // Get calendar object from calendarId
   const calendar = CalendarApp.getAllCalendars().filter(calendar => calendar.getId() === calendarId)[0];
-  console.log(`Evaluating calendar ${calendar.getTitle()} in the range of ${new Date(Number(startDate))} to ${new Date(Number(endDate))}`)
+  console.log(`Evaluating calendar ${calendar.getName()} in the range of ${new Date(Number(startDate))} to ${new Date(Number(endDate))}`)
 
 
   let currentDate = startDate;
   // TODO: Fix this Abbruchkriterium
   const allDevelopersDict = {};
   while (currentDate <= (endDate - 24 * 60 * 60 * 1000)) {
-    currentDate += 24 * 60 * 60 * 1000  // Move one day forward; Note that the startdate is exclusive
+    currentDate += 24 * 60 * 60 * 1000  // Move one day forward; Note that the startDate is exclusive
 
     // get the calender events
-    events = calendar.getEventsForDay(new Date(currentDate))
-    filteredEvents = events.filter(event => event.isAllDayEvent())
+    const events = calendar.getEventsForDay(new Date(currentDate))
+    let filteredEvents = events.filter(event => event.isAllDayEvent())
     filteredEvents = filteredEvents.filter(event => eventFilter(event.getTitle()))
     filteredEvents.forEach(event => {
       console.log(`On ${new Date(currentDate)} there is an event with the title ${event.getTitle()}`)
@@ -128,12 +128,12 @@ function setDevelopersSection() {
 
 
   // Make Dict to Array
-  allDevelopers = Object.entries(allDevelopersDict).map((event, _) => {
+  let allDevelopers = Object.entries(allDevelopersDict).map((event, _) => {
     return {title: event[0], id: event[1]}
   })
-  listOfAllDevelopers = []
+  let listOfAllDevelopers = []
 
-  var developerSwitches = allDevelopers
+  const developerSwitches = allDevelopers
       .map(({title, id}) => {
         listOfAllDevelopers.push(title)
         let isSelected = Object.keys(developerSwitchChoices).includes(title) ? developerSwitchChoices[title] : false
@@ -145,17 +145,17 @@ function setDevelopersSection() {
                 .setFieldName(title)
                 .setValue('True')
                 .setSelected(isSelected))
-      })
+      });
 
 
-  var nextButton = CardService.newTextButton()
+  const nextButton = CardService.newTextButton()
       .setText("Next ->")
       .setOnClickAction(CardService.newAction()
           .setFunctionName("saveDeveloperSwitchChoices")
-          .setParameters({listOfAllDevelopers: JSON.stringify(listOfAllDevelopers)}))
+          .setParameters({listOfAllDevelopers: JSON.stringify(listOfAllDevelopers)}));
 
 
-  card = CardService.newCardSection()
+  let card = CardService.newCardSection()
       .addWidget(changeDefaultsButton)
 
   developerSwitches.forEach(developerSwitch => card.addWidget(developerSwitch))
@@ -165,15 +165,15 @@ function setDevelopersSection() {
 }
 
 function countPairdaysSection() {
-  var changeDevelopersButton = CardService.newTextButton()
+  const changeDevelopersButton = CardService.newTextButton()
       .setText("<- Set Developers")
       .setOnClickAction(CardService.newAction()
-          .setFunctionName("handleChangeDevelopersButton"))
+          .setFunctionName("handleChangeDevelopersButton"));
 
-  var pairdayscount = countPairDaysFromProperties()
+  const pairdaysCount = countPairDaysFromProperties();
 
-  var pairdays = CardService.newDecoratedText()
-      .setText(`Pairdays: ${pairdayscount}`)
+  const pairdays = CardService.newDecoratedText()
+      .setText(`Pairdays: ${pairdaysCount}`);
 
   return CardService.newCardSection()
       .addWidget(changeDevelopersButton)
@@ -183,8 +183,8 @@ function countPairdaysSection() {
 function saveDeveloperSwitchChoices(event) {
   const userPropertyStore = PropertiesService.getUserProperties()
 
-  var developerSwitchChoicesString = userPropertyStore.getProperty('developerSwitchChoices')
-  var developerChoicesDict = {}
+  const developerSwitchChoicesString = userPropertyStore.getProperty('developerSwitchChoices');
+  let developerChoicesDict = {};
   if (developerSwitchChoicesString) {
     try {
       developerChoicesDict = JSON.parse(developerSwitchChoicesString)
@@ -193,17 +193,17 @@ function saveDeveloperSwitchChoices(event) {
     }
   }
 
-  listOfChoices = Object.keys(event.formInputs)
-  listOfAllDevelopers = JSON.parse(event.parameters['listOfAllDevelopers'])
+  let listOfChoices = Object.keys(event.formInputs)
+  let listOfAllDevelopers = JSON.parse(event.parameters['listOfAllDevelopers'])
 
   // save a dictionary with True / False for each possible developer
-  for (dev in listOfAllDevelopers) {
+  for (let dev in listOfAllDevelopers) {
     dev = listOfAllDevelopers[dev]
     developerChoicesDict[dev] = listOfChoices.includes(dev)
   }
 
   userPropertyStore.setProperty('developerSwitchChoices', JSON.stringify(developerChoicesDict))
-  userPropertyStore.setProperty('lastDeveloperSwitchesUpdate', +new Date())
+  userPropertyStore.setProperty('lastDeveloperSwitchesUpdate', (+new Date()).toString())
 
 
   return main()
@@ -212,13 +212,13 @@ function saveDeveloperSwitchChoices(event) {
 function handleStartTimeChange(event) {
   const userPropertyStore = PropertiesService.getUserProperties()
   var start_time = event.formInputs['date_field_start_time'][0]['msSinceEpoch']
-  userPropertyStore.setProperty('startdate', start_time)
+  userPropertyStore.setProperty('startDate', start_time)
 }
 
 function handleEndTimeChange(event) {
   const userPropertyStore = PropertiesService.getUserProperties()
   var end_time = event.formInputs['date_field_end_time'][0]['msSinceEpoch']
-  userPropertyStore.setProperty('enddate', end_time)
+  userPropertyStore.setProperty('endDate', end_time)
 }
 
 function handleDropDownChange(event) {
@@ -288,8 +288,8 @@ function countPairDays(calendarId, startTime, endTime, developerChoicesDict) {
 function countPairDaysFromProperties() {
   const userPropertyStore = PropertiesService.getUserProperties()
   var calendarId = userPropertyStore.getProperty('calendarId')
-  var startTime = Number(userPropertyStore.getProperty('startdate'))
-  var endTime = Number(userPropertyStore.getProperty('enddate'))
+  var startTime = Number(userPropertyStore.getProperty('startDate'))
+  var endTime = Number(userPropertyStore.getProperty('endDate'))
 
   var developerSwitchChoicesString = userPropertyStore.getProperty('developerSwitchChoices')
   var developerChoicesDict = {}
